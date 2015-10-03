e20c45c7eca1cdbe312c78929dedb58ef94dbed9
Ben Weber <benweber@student.ethz.ch>
2015-10-03 18:34:49 +0200

    Added skeleton of refinement module

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var refinement = require('./refinement');
 
 app.get('/', function(req, res){
 	res.send("Hello world");
@@ -10,5 +11,9 @@ app.get('/init', function(req, res){
 	res.send("init");
 });
 
+// TODO: call refinement.init with flats (JSON array) and res
+app.get('/another', refinement.another);
+app.get('/cheaper', refinement.cheaper);
+app.get('/closer', refinement.closer);
 
 app.listen(process.env.PORT || 8080);
