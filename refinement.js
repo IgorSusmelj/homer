53831ff3dae1a60a63d637cbaebff57f0e80ffa8
Ben Weber <benweber@student.ethz.ch>
2015-10-03 21:37:52 +0200

    Progressed with refinement and server skeleton

--- refinement.js
+++ refinement.js
@@ -1,32 +1,57 @@
 module.exports = {
-   initial : init,
+   init : init,
    another : another,
    cheaper : cheaper,
-   closer : closer
-}
-
-function init(flats, res) {
-   console.log("init");
-   res.send("init");
-}
-
-function another(req, res) {
-   console.log("another");
-   res.send("another");
-}
-
-function cheaper(req, res) {
-   console.log("cheaper");
-   res.send("cheaper");
-}
-
-function closer(req, res) {
-   console.log("closer");
-   res.send("closer");
+   closer : closer,
+   dummylist : getDummyList()
 }
 
 /* maps from session id (string) to Refinement instances */
-session = {}
+sessions = {};
+
+function init(flats, res) {
+   // TODO
+   // don't forget about the case where flats is empty!
+   res.send("OK init");
+}
+
+function fetchSession(req, res) {
+   sessionid = req.query.sessionid;
+   if (sessionid == undefined) {
+      res.send("Invalid sessionid (undefined)!");
+      return null;
+   }
+
+   refinement = sessions[sessionid];
+
+   if (refinement == undefined) {
+      res.send("Invalid sessionid (non-existent)!");
+      return null;
+   }
+
+   return refinement;
+}
+
+function another(req, res) {
+   if (null == (refinement = fetchSession(req, res)))
+      return;
+
+   res.send("OK another");
+}
+
+function cheaper(req, res) {
+   if (null == (refinement = fetchSession(req, res)))
+      return;
+
+   res.send("OK cheaper");
+}
+
+function closer(req, res) {
+   if (null == (refinement = fetchSession(req, res)))
+      return;
+
+   res.send("OK closer");
+}
 
 /**
  * Class to handle refinement events of the front end
@@ -43,33 +68,82 @@ session = {}
  */
 function Refinement(flats) {
 
-   /* all flats remaining (avoids outputing duplicates) */
+   /** all flats remaining (avoids outputing duplicates) */
    this.flats = flats;
-   /* the last selected flat (used as reference for cheaper & closer) */
-   this.current = null;
 
    /**
-    * Chooses an initial flat out of the set of flats that satisfy the initial
-    * requirements
+    * Picks an initial suggestion, removes it from flats and returns it
     */
-   this.initial = function() {
+   function initial(flats) {
+      // TODO: improve
+      return flats.pop();
    }
+
+   /**
+    * the last selected flat (used as reference for cheaper & closer)
+    */
+   this.current = initial(this.flats);
 
    /**
    * Finds another similar flat
    */
    this.another = function() {
+      return this.current = this.flats.pop();
    }
 
    /**
    * Finds cheaper similar flat
    */
    this.cheaper = function() {
+      // TODO
+      return this.current = this.flats.pop();
    }
 
    /**
    * Finds closer similar flat
    */
    this.closer = function() {
+      // TODO
+      return this.current = this.flats.pop();
    }
+}
+
+function arrayRemove(array, element) {
+}
+
+function getDummyList() {
+   return [
+      {
+         id : 1,
+         price : 1200,
+         img : 'some_image_url',
+         address : 'some address',
+         traveltime : 20,
+         rooms : 4
+      },
+      {
+         id : 2,
+         price : 1500,
+         img : 'some_image_url',
+         address : 'some address',
+         traveltime : 15,
+         rooms : 3.5
+      },
+      {
+         id : 3,
+         price : 1100,
+         img : 'some_image_url',
+         address : 'some address',
+         traveltime : 30,
+         rooms : 3.5
+      },
+      {
+         id : 4,
+         price : 1250,
+         img : 'some_image_url',
+         address : 'some address',
+         traveltime : 23,
+         rooms : 4
+      }
+   ];
 }
