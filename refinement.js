cf7cd4dced30df2c7a84eddd7c4bbc0030ea91a3
Ben Weber <benweber@student.ethz.ch>
2015-10-03 22:11:18 +0200

    Added static dummy flatscache for testing the front-end

--- refinement.js
+++ refinement.js
@@ -7,12 +7,17 @@ module.exports = {
 }
 
 /* maps from session id (string) to Refinement instances */
-sessions = {};
+sessions = {
+   "0" : getDummyFlatsCache()
+   };
 
 function init(flats, res) {
    // TODO
    // don't forget about the case where flats is empty!
-   res.send("OK init");
+
+   cache = getDummyFlatsCache();
+   cache["sessionid"] = "0";
+   res.send(cache);
 }
 
 function fetchSession(req, res) {
@@ -36,21 +41,21 @@ function another(req, res) {
    if (null == (refinement = fetchSession(req, res)))
       return;
 
-   res.send("OK another");
+   res.send(getDummyFlatsCache());
 }
 
 function cheaper(req, res) {
    if (null == (refinement = fetchSession(req, res)))
       return;
 
-   res.send("OK cheaper");
+   res.send(getDummyFlatsCache());
 }
 
 function closer(req, res) {
    if (null == (refinement = fetchSession(req, res)))
       return;
 
-   res.send("OK closer");
+   res.send(getDummyFlatsCache());
 }
 
 /**
@@ -146,4 +151,121 @@ function getDummyList() {
          rooms : 4
       }
    ];
+}
+
+function getDummyFlatsCache() {
+   return {
+   current : {
+      id : 1,
+      price : 1200,
+      img : 'http://foto.hrsstatic.com/fotos/0/3/256/256/80/000000/http%3A%2F%2Ffoto-origin.hrsstatic.com%2Ffoto%2F4%2F4%2F8%2F9%2F448922%2F448922_r_601772.jpg/9hil%2FhW3wIAlTZpshhnbaw%3D%3D/256,204/6/Aviva_Apartment-Hotel-Gross-Zimmern-Restaurant_Frhstcksraum-1-448922.jpg',
+      address : 'address of flat 1',
+      traveltime : 20,
+      rooms : 4
+      },
+   another : {
+      current : {
+         id : 2,
+         price : 1500,
+         img : 'http://foto.hrsstatic.com/fotos/0/3/256/256/80/000000/http%3A%2F%2Ffoto-origin.hrsstatic.com%2Ffoto%2F4%2F3%2F9%2F5%2F439565%2F439565_ap_580791.jpg/9qrQUuMWa4Vgg%2B%2BPzMyI3Q%3D%3D/256,251/6/Marsil_Apartment_Hotel-Koeln-Appartement-6-439565.jpg',
+         address : 'address of flat 1',
+         traveltime : 15,
+         rooms : 3.5
+         },
+      another : {
+         id : 3,
+         price : 1100,
+         img : 'http://foto.hrsstatic.com/fotos/0/3/256/256/80/000000/http%3A%2F%2Ffoto-origin.hrsstatic.com%2Ffoto%2F0%2F5%2F1%2F4%2F051497%2F051497_ap_125501.jpg/LbWY7WDaQzeyDZkItH1THQ%3D%3D/256,256/6/Birkebeineren_Hotel_Apartments-Lillehammer-Appartement-51497.jpg',
+         address : 'address of flat 1',
+         traveltime : 30,
+         rooms : 3.5
+         },
+      cheaper : {
+         id : 4,
+         price : 1250,
+         img : 'http://foto.hrsstatic.com/fotos/0/3/256/256/80/000000/http%3A%2F%2Ffoto-origin.hrsstatic.com%2Ffoto%2F4%2F1%2F2%2F7%2F412705%2F412705_ap_3037972.jpg/GwXxbYpGqcsiOLLbif5CyQ%3D%3D/256,192/6/Arinza_Apartment_Kingsley_Court-Liverpool-Appartement-7-412705.jpg',
+         address : 'address of flat 1',
+         traveltime : 23,
+         rooms : 4
+         },
+      closer : {
+         id : 5,
+         price : 1250,
+         img : 'some_image_url',
+         address : 'address of flat 1',
+         traveltime : 23,
+         rooms : 4
+         }
+      },
+   cheapaer : {
+      current : {
+         id : 2,
+         price : 1500,
+         img : 'http://foto.hrsstatic.com/fotos/0/3/256/256/80/000000/http%3A%2F%2Ffoto-origin.hrsstatic.com%2Ffoto%2F4%2F3%2F9%2F5%2F439565%2F439565_ap_580791.jpg/9qrQUuMWa4Vgg%2B%2BPzMyI3Q%3D%3D/256,251/6/Marsil_Apartment_Hotel-Koeln-Appartement-6-439565.jpg',
+         address : 'address of flat 1',
+         traveltime : 15,
+         rooms : 3.5
+         },
+      another : {
+         id : 3,
+         price : 1100,
+         img : 'http://foto.hrsstatic.com/fotos/0/3/256/256/80/000000/http%3A%2F%2Ffoto-origin.hrsstatic.com%2Ffoto%2F0%2F5%2F1%2F4%2F051497%2F051497_ap_125501.jpg/LbWY7WDaQzeyDZkItH1THQ%3D%3D/256,256/6/Birkebeineren_Hotel_Apartments-Lillehammer-Appartement-51497.jpg',
+         address : 'address of flat 1',
+         traveltime : 30,
+         rooms : 3.5
+         },
+      cheaper : {
+         id : 4,
+         price : 1250,
+         img : 'http://foto.hrsstatic.com/fotos/0/3/256/256/80/000000/http%3A%2F%2Ffoto-origin.hrsstatic.com%2Ffoto%2F4%2F1%2F2%2F7%2F412705%2F412705_ap_3037972.jpg/GwXxbYpGqcsiOLLbif5CyQ%3D%3D/256,192/6/Arinza_Apartment_Kingsley_Court-Liverpool-Appartement-7-412705.jpg',
+         address : 'address of flat 1',
+         traveltime : 23,
+         rooms : 4
+         },
+      closer : {
+         id : 5,
+         price : 1250,
+         img : 'some_image_url',
+         address : 'address of flat 1',
+         traveltime : 23,
+         rooms : 4
+         }
+
+      },
+   closer : {
+      current : {
+         id : 2,
+         price : 1500,
+         img : 'http://foto.hrsstatic.com/fotos/0/3/256/256/80/000000/http%3A%2F%2Ffoto-origin.hrsstatic.com%2Ffoto%2F4%2F3%2F9%2F5%2F439565%2F439565_ap_580791.jpg/9qrQUuMWa4Vgg%2B%2BPzMyI3Q%3D%3D/256,251/6/Marsil_Apartment_Hotel-Koeln-Appartement-6-439565.jpg',
+         address : 'address of flat 1',
+         traveltime : 15,
+         rooms : 3.5
+         },
+      another : {
+         id : 3,
+         price : 1100,
+         img : 'http://foto.hrsstatic.com/fotos/0/3/256/256/80/000000/http%3A%2F%2Ffoto-origin.hrsstatic.com%2Ffoto%2F0%2F5%2F1%2F4%2F051497%2F051497_ap_125501.jpg/LbWY7WDaQzeyDZkItH1THQ%3D%3D/256,256/6/Birkebeineren_Hotel_Apartments-Lillehammer-Appartement-51497.jpg',
+         address : 'address of flat 1',
+         traveltime : 30,
+         rooms : 3.5
+         },
+      cheaper : {
+         id : 4,
+         price : 1250,
+         img : 'http://foto.hrsstatic.com/fotos/0/3/256/256/80/000000/http%3A%2F%2Ffoto-origin.hrsstatic.com%2Ffoto%2F4%2F1%2F2%2F7%2F412705%2F412705_ap_3037972.jpg/GwXxbYpGqcsiOLLbif5CyQ%3D%3D/256,192/6/Arinza_Apartment_Kingsley_Court-Liverpool-Appartement-7-412705.jpg',
+         address : 'address of flat 1',
+         traveltime : 23,
+         rooms : 4
+         },
+      closer : {
+         id : 5,
+         price : 1250,
+         img : 'some_image_url',
+         address : 'address of flat 1',
+         traveltime : 23,
+         rooms : 4
+         }
+
+      }
+   };
 }
