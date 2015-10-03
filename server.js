dcf24e320fb4ec6848422e9f0ba611bec2fbd877
= <=>
2015-10-03 14:59:40 +0200

    changed homegate request according to example

--- server.js
+++ server.js
@@ -1,36 +1,82 @@
 var express = require('express');
-var request = require('request');
+var https = require('https');
 
 var app = express();
 
-	
+const API_KEY = 'ac22e38757948595929ae831aacadeb7';
+
+
+function _composeGet(param_sets) {
+  // compose GET parameters
+  get = [];
+
+  for (var k in param_sets) {
+    var set = param_sets[k];
+    for (var p in set) {
+      get.push(encodeURIComponent(p) + '=' + encodeURIComponent(set[p]));
+    }
+  }
+
+  return get.join('&');
+}
+
+
+function _homegateReq(path, callback) {
+  var req_params = {
+    protocol: "https:",
+    host: "api-2445581357976.apicast.io",
+    path: "/rs/real-estates" + path,
+    port: 443,
+    headers: {
+      'Accept': 'application/json',
+      'auth': API_KEY
+    }
+  };
+
+  req = https.request(req_params, function (resp) {
+    var str = '';
+
+    resp.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    resp.on('end', function () {
+      callback(JSON.parse(str));
+    });
+
+    resp.on('error', function () {
+      console.log('ERR: homegate API query failed');
+      callback(null);
+    });
+  });
+
+  req.end();
+}
+
+function search(search_params, callback) {
+  parameters = {
+    'language': 'en',
+    'chooseType': 'rentflat',
+  };
+  path = '?' + _composeGet([parameters, search_params]);
+
+  _homegateReq(path, callback);
+};			
 
 
 var getHomegate = function(){
 
-request('https://api-2445581357976.apicast.io:443/rs/real-estates/105240000?auth=ac22e38757948595929ae831aacadeb7', function (error, response, body) {
-    //Check for error
-    if(error){
-        return console.log('Error:', error);
-    }
-
-    //Check for right status code
-    if(response.statusCode !== 200){
-        return console.log('Invalid Status Code Returned:', response.statusCode);
-    }
-
-    //All is good. Print the body
-    console.log(body); // Show the HTML for the Modulus homepage.
-    return body;
-});
 }
 
 
 app.get('/init', function(req, res){
 
+	var response = search(null, callback);
 
-	res.send(getHomegate());
+	var callback = function(){
+		res.send(response);
 
+	}
 
 	//res.send("Nice guy");
 });
