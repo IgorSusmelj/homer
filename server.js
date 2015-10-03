205d578d262bf185b3fda1ae800362fb261cf94c
= <=>
2015-10-03 04:58:01 +0200

    moved to express

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-var http = require('http')
+/*var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
 }).listen(port);
+
+*/
+
+var express = require('express');
+var app = express();
+
+app.get('/init', function(req, res){
+	res.send("Nice guy");
+});
+
+app.listen(process.env.PORT || 8080);
